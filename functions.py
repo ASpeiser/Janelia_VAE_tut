a88c332d1e1268a310e2ef85f365c1e0467da68f
Artur Speiser <artur.speiser@caesar.de>
2016-10-06 19:29:47 +0200

    add download func.

--- functions.py
+++ functions.py
@@ -11,6 +11,44 @@ import numpy
 import pickle
 import sys
 import timeit
+import os
+import numpy as np
+
+def load_dataset():
+    # We first define a download function
+
+    from urllib.request import urlretrieve
+
+    def download(filename, source='http://yann.lecun.com/exdb/mnist/'):
+        print("Downloading %s" % filename)
+        urlretrieve(source + filename, filename)
+
+    # We then define functions for loading MNIST images and labels.
+    # For convenience, they also download the requested files if needed.
+    import gzip
+
+    def load_mnist_images(filename):
+        if not os.path.exists(filename):
+            download(filename)
+        # Read the inputs in Yann LeCun's binary format.
+        with gzip.open(filename, 'rb') as f:
+            data = np.frombuffer(f.read(), np.uint8, offset=16)
+        # The inputs are vectors now, we reshape them to monochrome 2D images,
+        # following the shape convention: (examples, channels, rows, columns)
+        data = data.reshape(-1, 1, 28, 28)
+        # The inputs come as bytes, we convert them to float32 in range [0,1].
+        # (Actually to range [0, 255/256], for compatibility to the version
+        # provided at http://deeplearning.net/data/mnist/mnist.pkl.gz.)
+        return data / np.float32(256)
+    
+    X_train = load_mnist_images('train-images-idx3-ubyte.gz')
+    X_test = load_mnist_images('t10k-images-idx3-ubyte.gz')
+
+    # We reserve the last 10000 training examples for validation.
+
+    # We just return all the arrays in order, as expected in main().
+    # (It doesn't matter how we do this as long as we can read them again.)
+    return X_train, X_test
 
 def load_data_shared(dataset):  
      
